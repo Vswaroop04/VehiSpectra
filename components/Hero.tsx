fb91620b6472030f09e5ddd95245dddb1a518d88
vishnuoruphones <vishnu@oruphones.com>
2023-06-21 23:56:35 +0530

    added handle scroll

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,11 @@
+"use-client"
 import React from "react";
 import CustomButton from "./CustomButton";
 
 const Hero = () => {
+  const handleScroll = () => {
+
+  }
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x"></div>
@@ -17,7 +21,7 @@ const Hero = () => {
       <CustomButton
         title="Explore-Cars"
         containerStyles="bg-primary-blue text-white rounded-full mt-10"
-        handleClick={() => {}}
+        handleClick={handleScroll}
       />
     </div>
   );
